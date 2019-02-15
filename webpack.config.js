22e538e947a81aa71eb47bcc3dabc42e5e7dca69
coffeegoddd <dbeaubrown@gmail.com>
2019-02-15 10:29:30 -0800

    basic setup including installing webpack and wrting basic travis build yml

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, './client/public/dist'),
+    filename: 'bundle.js'
+  },
+  // devServer: {
+  //   contentBase: './dist'
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        ],
+        include: /\.module\.css$/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  }
+}
+
+module.exports = config;
