877b8638514b44690c4664fc3de035514ab4aacd
coffeegoddd <dbeaubrown@gmail.com>
2019-02-15 15:52:13 -0800

    wrote tests for all crud routes and created routes that pass all tests

---
+++ db/dbHelpers.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+
+module.exports = {
+  getPlayerInfo: async (username, collection) => {
+    const data = await collection.findOne({ username });
+    return data;
+  },
+  getAllPlayers: async (collection) => {
+    const data = await collection.find({}).toArray();
+    return data;
+  },
+  createPlayer: async (newPlayer, collection) => {
+    const data = await collection.insertOne(newPlayer, (err, result) => {
+      assert.equal(null, err);
+      assert.equal(1, result.insertedCount);
+    });
+    return data;
+  },
+  updatePlayerInfo: async (_id, newPlayerInfo, collection) => {
+    const data = await collection.updateOne({ _id }, { $set: newPlayerInfo }, (err, result) => {
+      assert.equal(null, err);
+      assert.equal(1, result.matchedCount);
+      assert.equal(1, result.modifiedCount);
+    });
+    return data;
+  },
+  deletePlayer: async (_id, collection) => {
+    const data = await collection.deleteOne({ _id }, (err, result) => {
+      assert.equal(null, err);
+      assert.equal(1, result.deletedCount);
+    });
+    return data;
+  },
+};
